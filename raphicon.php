9253654b28460202a181450962e25a97c6b7b5b8
rahendz <rahendz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-13 12:56:04 +0000

    test 1.0
    git-svn-id: https://plugins.svn.wordpress.org/raphicon/tags/1.0@913287 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ raphicon.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Plugin Name: RAphicon
+ * Plugin URI: http://rahendz.web.id
+ * Description: Bootstrap 3 Glpyhicon And Wordpress Dashicon Plugin - Is a dropdown button on visual editor that will insert Bootstrap 3.1.1 Glyphicon or Wordpress Dashicons into post/page on a shortcode and customizable size and/or color.
+ * Version: 1.0
+ * Author: rahendz
+ * Author URI: http://rahendz.web.id/
+ * License: GPL2
+ */
+
+/*  Copyright 2014  rahendz  (email : me@rahendz.web.id)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! class_exists ( 'RAphicon' ) AND defined ( 'ABSPATH' ) ) :
+	require_once ABSPATH . "wp-includes/pluggable.php";
+	define ( 'RAPHICON_URL', plugin_dir_url ( __FILE__ ) );
+	define ( 'RAPHICON_PATH', dirname ( __FILE__ ) );
+	define ( 'RAPHICON_URI', $_SERVER['REQUEST_URI'] );
+	class RAphicon
+	{
+		public function __construct()
+		{
+			add_action ( 'init', array ( &$this, '__initiate' ) );
+			add_action ( 'admin_enqueue_scripts', array ( &$this, '__enqueue_scripts' ) );
+			add_action ( 'wp_enqueue_scripts', array ( &$this, '__enqueue_script' ) );
+			add_shortcode( 'glyphicon', array ( &$this, '__glyphicon' ) );
+			add_shortcode( 'dashicons', array ( &$this, '__dashicons' ) );
+		}
+		public function __initiate()
+		{
+			ob_start();
+			if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') ) { return; }
+			if ( get_user_option('rich_editing') == 'true' ) 
+			{
+				add_filter( 'mce_external_plugins', array ( &$this, '__plugins' ) );
+				add_filter( 'mce_buttons', array ( &$this, '__buttons' ) );
+			}
+		}
+		public function __buttons ( $buttons )
+		{
+			array_push ( $buttons, "|", 'RAphicon' );
+			return $buttons;
+		}
+		public function __plugins ( $plugin_array )
+		{
+			$plugin_array['RAphicon'] = sprintf ( "%s/%s/%s", RAPHICON_URL, "js", "raphicon.tinymce.js" );
+			return $plugin_array;
+		}
+		public function __enqueue_scripts()
+		{
+			wp_enqueue_style ( 'raphicon', sprintf ( "%s/%s/%s", trim ( RAPHICON_URL, "/" ), 'css', 'raphicon.min.css' ), array(), '1.0.0' );
+		}
+		public function __enqueue_script()
+		{
+			wp_enqueue_style ( 'dashicons' );
+			wp_enqueue_style ( 'raphicon', sprintf ( "%s/%s/%s", trim ( RAPHICON_URL, "/" ), 'css', 'raphicon.min.css' ), array('dashicons'), '1.0.0' );
+		}
+		public function __glyphicon ( $atts )
+		{
+			extract ( shortcode_atts ( 
+				array ( 'type' => 'none', 'size' => 'inherit', 'color' => 'inherit' ), $atts, 'glyphicon'
+				) );
+			return '<span class="glyphicon glyphicon-' . $type . '" style="font-size:' . $size . ';color:' . $color . ';"></span>';
+		}
+		public function __dashicons ( $atts )
+		{
+			extract ( shortcode_atts ( 
+				array ( 'type' => 'none', 'size' => 'inherit', 'color' => 'inherit' ), $atts, 'dashicons'
+				) );
+			return '<span class="dashicons dashicons-' . $type . ' raphicon" style="font-size:' . $size . ';color:' . $color . ';"></span>';
+		}
+	}
+	// register_activation_hook ( __FILE__, array ( array ( &$RAphicon, '__init' ), array ( "activate" ) ) );
+	// register_deactivation_hook ( __FILE__, array ( array ( &$RAphicon, '__init' ), array ( "deactivate" ) ) );
+	$RAphicon = new RAphicon();
+endif;
---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Plugin Name ===
+Contributors: rahendz
+Donate link: http://example.com/
+Tags: icons, tinymce, ajax, bootstrap, twitter, twitter bootstrap, glyphicons, visual editor, dahsicons, wordpress
+Requires at least: 3.0.1
+Tested up to: 3.9
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Bootstrap 3 Glpyhicon And Wordpress Dashicon Plugin
+
+== Description ==
+
+Is a dropdown button on visual editor that will insert Bootstrap 3.1.1 Glyphicon or Wordpress Dashicons into post/page on a shortcode and customizable size and/or color.
+
+Instructions for use:
+
+Method A (Visual Editor - Preferred):
+
+1. Edit/Add any Page or Post
+
+2. In the visual editor you will see a "RAphicon" dropdown button. Click the button
+
+3. You will now be presented with a selection menu.
+
+4. Simply select any icon and the shortcode will be placed into your post/page! OR if you tired to scrolling down the dropdown menu you use Browser Find Feature (ctrl+F) instead, just find using "D" (for dashicons) or "G" (for glyphicon) right before the icon_name of icon. example: "D Admin-post" without quote.
+
+
+Method B (Manual Shortcode):
+Insert shortcode into your post.
+`[glyphicon type="icon_name"]` for default glyphicon shortcode and
+`[dashicons type="icon_name"]` for default dashicons shortcode
+You could put size and color attribute also:
+`[glyphicon type="icon_name" size="size_with_units" color="color_with_hashtag"]`
+it will looks like this:
+`[glyphicon type="asterisk" size="20em" color="#999999"]`
+
+
+ Method C (Manual into Theme):
+Since this loads the necessary fonts and css for the Glyphicon / Dashicons you may put them anywhere using the standardized code:
+`<span class="glyphicon glyphicon-icon_name"></span>` or `<span class="dashicons dashicons-icon_name"></span>`
+
+== Installation ==
+
+1. Upload contents of `raphicon.zip` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Go to the visual editor for any page or post and use the "RAphicon" dropdown button.
+
+== Frequently Asked Questions ==
+= Not Yet =
+
+== Screenshots ==
+
+1. New Visual Editor Button
+2. Icons Selection List
+3. Custom attribute
+
+== Changelog ==
+= 1.0 =
+* Initial release.
+
+== Upgrade Notice ==
+= 1.0 =
+Adding Dashicon
+
+== Arbitrary section ==
+Don't know what it is section
+
+== Donations ==
+None
